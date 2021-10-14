62d14306bae5972956e488b2c44be606c74d3927
Jolersion <jolersion@hotmail.com>
2021-10-14 11:50:45 -0300

    214 Campos transientes

--- Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
+++ Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
@@ -4,19 +4,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 
 @SuppressWarnings("serial")
 @Entity
 public class Usuario extends GenericDomain {
 	@Column(length = 32, nullable = false)
 	private String senha;
-	
+
 	@Column(nullable = false)
 	private Character tipo;
-	
+
 	@Column(nullable = false)
 	private Boolean ativo;
-	
+
 	@OneToOne
 	@JoinColumn(nullable = false)
 	private Pessoa pessoa;
@@ -33,12 +34,38 @@ public class Usuario extends GenericDomain {
 		return tipo;
 	}
 
+	@Transient
+	public String getTipoFormatado() {
+		String tipoFormatado = null;
+
+		if (tipo == 'A') {
+			tipoFormatado = "Administrador";
+		} else if (tipo == 'B') {
+			tipoFormatado = "Balconista";
+		} else if (tipo == 'G') {
+			tipoFormatado = "Gerente";
+		}
+		
+		return tipoFormatado;
+	}
+
 	public void setTipo(Character tipo) {
 		this.tipo = tipo;
 	}
 
 	public Boolean getAtivo() {
 		return ativo;
+	}
+	
+	@Transient
+	public String getAtivoFormatado(){
+		String ativoFormatado = "Não";
+		
+		if(ativo){
+			ativoFormatado = "Sim";
+		}
+		
+		return ativoFormatado;
 	}
 
 	public void setAtivo(Boolean ativo) {
